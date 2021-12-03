10a09703acd2712ce4c6e29823cc6bd5907fe008
Tachpol Posaphiwat <pond.posa@gmail.com>
2021-12-02 22:13:02 -0800

    left, right, and center options hard-coded. ready for further testing and calibration.

--- a1_soccer_traj/src/a1_soccer_opti_left.py
+++ a1_soccer_traj/src/a1_soccer_opti_left.py
@@ -471,14 +471,10 @@ if __name__ == "__main__":
 	init_ef_pos = np.array([0.183, -0.13205, 0, 0, 0, 0])
 	a1_kin_opti.set_init_ef_pos(init_ef_pos)
 
-    # # left
-	final_ef_pos = np.array([0.25, -0.13205, 0.16, 0, -0.6*np.pi, 0]) + np.array([0, 0.1, 0, 0, 0, 0])
-
-    # # center
-    # final_ef_pos = np.array([0.25, -0.13205, 0.16, 0, -0.6*np.pi, 0])
-
-    # right
-    # final_ef_pos = np.array([0.25, -0.13205, 0.16, 0, -0.6*np.pi, 0]) + np.array([0, -.1, 0, 0, 0, 0])
+    ## Uncomment to choose direction to kick (1. left, 2. center, 3. right)
+	# final_ef_pos = np.array([0.25, -0.13205, 0.16, 0, -0.6*np.pi, 0]) + np.array([0, 0.1, 0, 0, 0, 0])
+	# final_ef_pos = np.array([0.25, -0.13205, 0.16, 0, -0.6*np.pi, 0]) + np.array([0, 0, 0, 0, 0, 0])
+	final_ef_pos = np.array([0.25, -0.13205, 0.16, 0, -0.6*np.pi, 0]) + np.array([0, -0.1, 0, 0, 0, 0])
 
 	a1_kin_opti.set_final_ef_pos(final_ef_pos)
 	final_ef_vel = np.array([0, 0, 0])
